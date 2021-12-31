ab4fe0a62f4f4143b78beac09c3a1a41262383b3
jcorporation <mail@jcgames.de>
2021-12-31 19:24:41 +0100

    Upd: layout

--- assets/js/radiodb.js
+++ assets/js/radiodb.js
@@ -14,23 +14,19 @@ document.getElementById('searchstr').addEventListener('keyup', function(event) {
       if (webradios[key].PLAYLIST.toLowerCase().indexOf(searchstr) > -1) {
         i++;
         const div = document.createElement('div');
-        div.classList.add('searchResult');
         const pic = webradios[key].EXTIMG.indexOf('http:') === 0 ||
             webradios[key].EXTIMG.indexOf('https:') === 0 ?
                 webradios[key].EXTIMG : 'publish/pics/' + webradios[key].EXTIMG;
         div.innerHTML =
-          '<div>' + 
-            '<h3></h3>' +
             '<table>' +
-              '<caption>' + webradios[key].PLAYLIST + '</caption>' +
-              '<tbody>' +
+                '<caption>' + webradios[key].PLAYLIST + '</caption>' +
+                '<tbody>' +
                 '<tr><td rowspan="4"><img src="' + pic + '" class="stationImage"/></td><td>Genre</td><td>' + webradios[key].EXTGENRE + '</td></tr>' +
                 '<tr><td>Country</td><td>' + webradios[key].COUNTRY + '/' + webradios[key].LANGUAGE + '</td></tr>' +
                 '<tr><td>Homepage</td><td><a target="_blank" href="' + webradios[key].HOMEPAGE + '">' + webradios[key].HOMEPAGE + '</a></td></tr>' +
                 '<tr><td>Playlist</td><td><a target="_blank" href="publish/webradios/' + key + '">Get playlist</a></td></tr>' +
-              '</tbody>' +
-            '</table>' +
-          '</div>';
+                '</tbody>' +
+            '</table>';
         resultEl.appendChild(div);
       }
     }
