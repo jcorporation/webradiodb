66ebf4c6814c7e8c42049c23dd759f5210b22097
jcorporation <mail@jcgames.de>
2022-01-02 17:37:49 +0100

    Upd: add modify/delete links Fix: prevent xss

--- docs/assets/js/radiodb.js
+++ docs/assets/js/radiodb.js
@@ -1,6 +1,10 @@
 const resultEl = document.getElementById('result');
 document.getElementById('stationCount').textContent = Object.keys(webradios).length;
 
+const issueUri = 'https://github.com/jcorporation/webradiodb/issues/new';
+const issueDelete = '?labels=DeleteWebradio&template=delete-webradio.yml';
+const issueModify = '?labels=ModifyWebradio&template=modify-webradio.yml';
+
 document.getElementById('searchstr').addEventListener('keyup', function(event) {
   if (event.key === 'Enter') {
     const searchstr = event.target.value.toLowerCase();
@@ -19,15 +23,48 @@ document.getElementById('searchstr').addEventListener('keyup', function(event) {
                 webradios[key].EXTIMG : 'db/pics/' + webradios[key].EXTIMG;
         div.innerHTML =
             '<table>' +
-                '<caption>' + webradios[key].PLAYLIST + '</caption>' +
-                '<tbody>' +
-                '<tr><td rowspan="5"><img src="' + pic + '" class="stationImage"/></td><td>Genre</td><td>' + webradios[key].EXTGENRE + '</td></tr>' +
-                '<tr><td>Country</td><td>' + webradios[key].COUNTRY + '/' + webradios[key].LANGUAGE + '</td></tr>' +
-                '<tr><td>Homepage</td><td><a target="_blank" href="' + webradios[key].HOMEPAGE + '">' + webradios[key].HOMEPAGE + '</a></td></tr>' +
-                '<tr><td>Playlist</td><td><a target="_blank" href="db/webradios/' + key + '">Get playlist</a></td></tr>' +
-                '<tr><td colspan="2">' + webradios[key].DESCRIPTION + '</td></tr>' +
-                '</tbody>' +
+              '<caption></caption>' +
+              '<tbody>' +
+                '<tr>' +
+                  '<td rowspan="6"><img src="" class="stationImage"/></td><td>Genre</td>' +
+                  '<td class="genre"></td></tr>' +
+                '<tr><td>Country</td><td class="country"></td></tr>' +
+                '<tr><td>Homepage</td><td><a class="homepage" target="_blank" href=""></a></td></tr>' +
+                '<tr><td>Stream URI</td><td><input type="text" value=""/></td></tr>' +
+                '<tr><td>Playlist</td><td><a class="playlist" target="_blank" href="">Get playlist</a></td></tr>' +
+                '<tr><td colspan="2" class="description"></td></tr>' +
+              '</tbody>' +
+              '<tfoot>' +
+                '<tr><td colspan="3">' +
+                  '<a class="modify" href="">Modify</a>&nbsp;&nbsp;<a class="delete" href="">Delete</a>' +
+                '</td></tr>' +
+              '</tfoot>' +
             '</table>';
+        div.getElementsByTagName('caption')[0] = webradios[key].PLAYLIST;
+        div.getElementsByTagName('img')[0].src = pic;
+        div.getElementsByClassName('genre')[0].textContent = webradios[key].EXTGENRE;
+        div.getElementsByClassName('country')[0].textContent = webradios[key].COUNTRY + ' / ' + webradios[key].LANGUAGE;
+        div.getElementsByClassName('homepage')[0].href = webradios[key].HOMEPAGE;
+        div.getElementsByClassName('homepage')[0].textContent = webradios[key].HOMEPAGE;
+        div.getElementsByTagName('input')[0].value = webradios[key].streamUri;
+        div.getElementsByClassName('playlist')[0].href = 'db/webradios/' + key;
+        div.getElementsByClassName('description')[0].textContent = webradios[key].DESCRIPTION;
+
+        div.getElementsByClassName('modify')[0].href =
+          issueUri + issueDelete + '&title=' + encodeURIComponent('[Modify Webradio]: ' + webradios[key].PLAYLIST) +
+            '&modifyWebradio=' + encodeURIComponent(webradios[key].streamUri) +
+            '&name=' + encodeURIComponent(webradios[key].PLAYLIST) +
+            '&streamuri=' + encodeURIComponent(webradios[key].streamUri) +
+            '&genre=' + encodeURIComponent(webradios[key].EXTGENRE) +
+            '&homepage=' + encodeURIComponent(webradios[key].HOMEPAGE) +
+            '&image=' + encodeURIComponent(webradios[key].EXTIMG) +
+            '&country=' + encodeURIComponent(webradios[key].COUNTRY) +
+            '&language=' + encodeURIComponent(webradios[key].LANGUAGE) +
+            '&description=' + encodeURIComponent(webradios[key].DESCRIPTION);
+
+        div.getElementsByClassName('delete')[0].href =
+          issueUri + issueDelete + '&title=' + encodeURIComponent('[Delete Webradio]: ' + webradios[key].PLAYLIST) +
+            '&deleteWebradio=' + encodeURIComponent(webradios[key].streamUri);
         resultEl.appendChild(div);
       }
     }
